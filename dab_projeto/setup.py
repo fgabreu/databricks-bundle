fcae4c9374897425fa8480fa8311b7737195ba5a
˜Felipe˜ <eng.felipeabreu@gmail.com>
2025-10-19 10:36:11 -0300

    Updated version in setup.py

--- dab_projeto/setup.py
+++ dab_projeto/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dab_projeto",
-    version="0.0.1",
+    version="0.0.2",
     description="This contains the code in the ./src directory of the project",
     author="Felipe Abreu",
     packages=find_packages(where="./src"),
